e4b0a5b34e3d6611e0c109b88e706273c2cff148
cmsalem <csalem3@cox.net>
2018-11-08 12:58:58 -0700

    Change Out put to Hello Pluto

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Mercury!");
+            Console.WriteLine("Hello Pluto!");
         }
     }
 }
