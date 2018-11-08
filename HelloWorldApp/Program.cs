8c0177bb0e3d7c9a47f277f093284b6380c3aaf6
cmsalem <csalem3@cox.net>
2018-11-08 14:59:51 -0700

    Add new code to feature 1

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -10,20 +10,13 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Mars!");
-            //Code was added in VS
+                       //Code was added in VS
             //Another code change for VS
             
 
             //Call Feature1
 
-            public int Add()
-            {
-            int x1 = 1;
-            int x2 = 2;
-            int sum = x1 + x2;
-            return sum;
-            }
+
 
         }
     }
