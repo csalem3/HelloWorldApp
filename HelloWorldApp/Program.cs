b12d1b7df4ee94177d3048483e4275f240df8b12
cmsalem <csalem3@cox.net>
2018-11-08 14:55:11 -0700

    Added the ADD Methon

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -17,6 +17,14 @@ namespace HelloWorldApp
 
             //Call Feature1
 
+            public int Add()
+            {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+            }
+
         }
     }
 }
