0e945b27c98e7cc18922dc2b2cb82af7d95565d7
cmsalem <csalem3@cox.net>
2018-11-08 13:03:52 -0700

    Change output to Hello Saturn

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Pluto!");
+            Console.WriteLine("Hello Saturn!");
         }
     }
 }
