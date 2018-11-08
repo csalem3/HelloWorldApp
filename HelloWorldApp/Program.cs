f28f52f394bc6efb9ea2b582cbce4bdfe8a233f4
cmsalem <csalem3@cox.net>
2018-11-08 12:39:29 -0700

    Change outmessage to Hello USA

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Earth!");
+            Console.WriteLine("Hello USA!");
         }
     }
 }
